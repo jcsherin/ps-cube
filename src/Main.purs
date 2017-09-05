d7f0b2d7e06a411a85631ae2d8d210d7d86c1ca9
Sherin C <sherin@protoship.io>
2017-09-05 20:00:28 +0530

    Perform a simple side effect on canvas by drawing a line

--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,20 @@
 module Main where
 
 import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+import Control.Monad.Eff (Eff)
+import Data.Int (toNumber)
+import Data.Maybe (Maybe(..))
+import Graphics.Canvas (CANVAS, Context2D, beginPath, closePath, getCanvasElementById, getContext2D, lineTo, moveTo, setStrokeStyle, stroke)
+
+main :: forall e. Partial => Eff (canvas :: CANVAS | e) Context2D
 main = do
-  log "Hello sailor!"
+  Just canvas <- getCanvasElementById "canvas"
+  ctx <- getContext2D canvas
+  _ <- setStrokeStyle "#ff0000" ctx
+  _ <- beginPath ctx
+  _ <- moveTo ctx (toNumber 10) (toNumber 10)
+  _ <- lineTo ctx (toNumber 200) (toNumber 200)
+  _ <- closePath ctx
+  stroke ctx
+
