ebe8b1f6508d7f301a2c69dae8e780fa9a68c918
Sherin C <sherin@protoship.io>
2017-09-06 14:59:10 +0530

    Animation loop with requestAnimationFrame continuation call

--- src/Main.purs
+++ src/Main.purs
@@ -3,18 +3,29 @@ module Main where
 import Prelude
 
 import Control.Monad.Eff (Eff)
+import DOM (DOM)
+import DOM.HTML (window)
+import DOM.HTML.Window (requestAnimationFrame)
 import Data.Int (toNumber)
 import Data.Maybe (Maybe(..))
 import Graphics.Canvas (CANVAS, Context2D, beginPath, closePath, getCanvasElementById, getContext2D, lineTo, moveTo, setStrokeStyle, stroke)
 
-main :: forall e. Partial => Eff (canvas :: CANVAS | e) Context2D
-main = do
-  Just canvas <- getCanvasElementById "canvas"
-  ctx <- getContext2D canvas
-  _ <- setStrokeStyle "#ff0000" ctx
+animate :: forall e. Context2D -> Eff (dom :: DOM, canvas :: CANVAS | e) Unit
+animate ctx = void do
+  drawTestLine ctx
+  window >>= requestAnimationFrame (animate ctx)
+
+drawTestLine :: forall e. Context2D -> Eff (dom :: DOM, canvas :: CANVAS | e) Unit
+drawTestLine ctx = void do
+  _ <- setStrokeStyle "#ff0000" ctx >>= beginPath
   _ <- beginPath ctx
   _ <- moveTo ctx (toNumber 10) (toNumber 10)
   _ <- lineTo ctx (toNumber 200) (toNumber 200)
   _ <- closePath ctx
   stroke ctx
 
+main :: forall e. Partial => Eff (dom :: DOM, canvas :: CANVAS | e) Unit
+main = void do
+  Just canvas <- getCanvasElementById "canvas"
+  ctx <- getContext2D canvas
+  animate ctx
