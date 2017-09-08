4918c47c47a2bab25c4133b89c29f4e5a1d36ef8
Sherin C <sherin@protoship.io>
2017-09-08 22:31:36 +0530

    Type Point2D, orthographic projection from 3D -> 2D

--- src/Geometry/Point.purs
+++ src/Geometry/Point.purs
@@ -9,11 +9,26 @@ type Point = { x :: Number
              , z :: Number
              }
 
+type Point2D = { x :: Number
+               , y :: Number
+               }
+
 point :: Number -> Number -> Number -> Point
-point x y z = { x: x, y: y, z: z }
+point x y z = { x, y, z }
 
 showPoint :: Point -> String
 showPoint p = "(" <>
-  toString p.x <> "," <>
-  toString p.y <> "," <>
+  toString p.x <> ", " <>
+  toString p.y <> ", " <>
   toString p.z <> ")"
+
+point2D :: Number -> Number -> Point2D
+point2D x y = { x, y }
+
+showPoint2D :: Point2D -> String
+showPoint2D p = "(" <>
+  toString p.x <> ", " <>
+  toString p.y <> ")"
+
+orthographic :: Point -> Point2D
+orthographic p = point2D p.x p.y
