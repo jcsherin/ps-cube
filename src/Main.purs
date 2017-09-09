ebbc5c49f8eee2a530b4d3c57c52a7f416e451ce
Sherin C <sherin@protoship.io>
2017-09-09 20:25:02 +0530

    Draw cube wireframe by drawing a line through it's vertices

--- src/Main.purs
+++ src/Main.purs
@@ -3,17 +3,38 @@ module Main where
 import Prelude
 
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE, log)
 import DOM (DOM)
 import DOM.HTML (window)
 import DOM.HTML.Window (requestAnimationFrame)
+import Data.Array (head, tail)
+import Data.Foldable (for_)
 import Data.Int (toNumber)
-import Data.Maybe (Maybe(..))
-import Graphics.Canvas (CANVAS, Context2D, beginPath, closePath, getCanvasElementById, getContext2D, lineTo, moveTo, setStrokeStyle, stroke)
+import Data.Maybe (Maybe(..), fromJust)
+import Geometry.Cube (faces)
+import Geometry.Point (orthographic, showPoint2D)
+import Graphics.Canvas (CANVAS, Context2D, beginPath, clearRect, closePath, getCanvasElementById, getContext2D, lineTo, moveTo, setStrokeStyle, stroke, translate)
+import Partial.Unsafe (unsafePartial)
 
-animate :: forall e. Context2D -> Eff (dom :: DOM, canvas :: CANVAS | e) Unit
+animate :: forall e. Context2D -> Eff (console :: CONSOLE, canvas :: CANVAS, dom :: DOM | e) Unit
 animate ctx = void do
-  drawTestLine ctx
+  drawCube ctx
   window >>= requestAnimationFrame (animate ctx)
+
+drawCube :: forall e. Context2D -> Eff (console :: CONSOLE, canvas :: CANVAS, dom :: DOM | e) Unit
+drawCube ctx = void $ do
+  let projected = map orthographic <$> faces 150.0
+  for_ projected \face -> do
+
+    let hd = unsafePartial $ fromJust $ head face
+    _ <- moveTo ctx hd.x hd.y
+
+    let tl = unsafePartial $ fromJust $ tail face
+    for_ tl \p -> do
+      lineTo ctx p.x p.y
+
+    _ <- closePath ctx
+    stroke ctx
 
 drawTestLine :: forall e. Context2D -> Eff (dom :: DOM, canvas :: CANVAS | e) Unit
 drawTestLine ctx = void do
@@ -24,8 +45,12 @@ drawTestLine ctx = void do
   _ <- closePath ctx
   stroke ctx
 
-main :: forall e. Partial => Eff (dom :: DOM, canvas :: CANVAS | e) Unit
+main :: forall e. Partial => Eff (console :: CONSOLE, canvas :: CANVAS, dom :: DOM | e) Unit
 main = void do
   Just canvas <- getCanvasElementById "canvas"
   ctx <- getContext2D canvas
+  _ <- clearRect ctx { x: 0.0, y: 0.0, w: 600.0, h: 400.0}
+  _ <- translate { translateX: 300.0, translateY: 200.0} ctx
+  _ <- setStrokeStyle "rgba(0, 0, 0, 0.3)" ctx
+
   animate ctx
