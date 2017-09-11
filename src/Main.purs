fae88f8e1b8464bfe08ac901230fe78cec96409f
Sherin C <sherin@protoship.io>
2017-09-11 21:12:17 +0530

    Extract state into a global ref

--- src/Main.purs
+++ src/Main.purs
@@ -26,26 +26,33 @@ import Graphics.Canvas (CANVAS, Context2D, beginPath, clearRect, closePath, getC
 import Math (abs, pi, round)
 import Partial.Unsafe (unsafePartial)
 
+type State = {  dragged :: Boolean
+              , prevX :: Int
+              , prevY :: Int
+              , deltaX :: Number
+              , deltaY :: Number
+              }
+
 decelerate :: Number -> Number -> Number
 decelerate rad factor = if (abs rad) > 0.009
   then rad * factor
   else 0.0
 
-animate :: forall e. Context2D -> Ref Number -> Ref Number -> Ref Boolean -> Array (Array Point) -> Eff (ref :: REF, console :: CONSOLE, canvas :: CANVAS, dom :: DOM | e) Unit
-animate ctx deltaX deltaY dragged cube = void do
+animate :: forall e. Context2D -> Ref State -> Array (Array Point) -> Eff (ref :: REF, console :: CONSOLE, canvas :: CANVAS, dom :: DOM | e) Unit
+animate ctx state cube = void do
   _ <- clearRect ctx { x: -300.0, y: -200.0, w: 600.0, h: 400.0}
 
-  dx <- readRef deltaX
-  dy <- readRef deltaY
-  drg <- readRef dragged
-  let rotated = map (rotateX dy) <$> map (rotateY dx) <$> cube
-
+  current <- readRef state
+  let rotated = map (rotateX current.deltaY) <$> map (rotateY current.deltaX) <$> cube
   drawCube ctx rotated
 
-  modifyRef deltaX (\x -> decelerate x 0.95)
-  modifyRef deltaY (\y -> decelerate y 0.95)
-
-  window >>= requestAnimationFrame (animate ctx deltaX deltaY dragged rotated)
+  modifyRef state (\s -> {  dragged : true
+                          , prevX   : s.prevX
+                          , prevY   : s.prevY
+                          , deltaX  : decelerate s.deltaX 0.95
+                          , deltaY  : decelerate s.deltaY 0.95
+                          })
+  window >>= requestAnimationFrame (animate ctx state rotated)
 
 drawCube :: forall e. Context2D -> Array (Array Point) -> Eff (console :: CONSOLE, canvas :: CANVAS, dom :: DOM | e) Unit
 drawCube ctx cube = void $ do
@@ -62,28 +69,38 @@ drawCube ctx cube = void $ do
     _ <- closePath ctx
     stroke ctx
 
-lock :: forall e. Ref Boolean -> Ref Int -> Ref Int -> MouseEvent -> Eff (ref :: REF, console :: CONSOLE | e) Unit
-lock dragged prevX prevY e = do
-  modifyRef dragged (\l -> true)
-  modifyRef prevX (\x -> clientX e)
-  modifyRef prevY (\y -> clientY e)
+lock :: forall e. Ref State -> MouseEvent -> Eff (ref :: REF, console :: CONSOLE | e) Unit
+lock state e = do
+  current <- readRef state
+  modifyRef state (\s -> {  dragged : true
+                          , prevX   : clientX e
+                          , prevY   : clientY e
+                          , deltaX  : s.deltaX
+                          , deltaY  : s.deltaY
+                          })
 
-rotate :: forall e. Ref Boolean -> Ref Int -> Ref Int -> Ref Number -> Ref Number -> MouseEvent -> Eff (ref :: REF, console :: CONSOLE | e) Unit
-rotate dragged prevX prevY deltaX deltaY e = do
-  drg <- readRef dragged
-  if drg
+rotate :: forall e. Ref State -> MouseEvent -> Eff (ref :: REF, console :: CONSOLE | e) Unit
+rotate state e = do
+  current <- readRef state
+  if current.dragged == true
     then do
-      px <- readRef prevX
-      py <- readRef prevY
-      modifyRef deltaX (\x -> toNumber (clientX e - px) * pi / 180.0)
-      modifyRef deltaY (\y -> toNumber (- (clientY e - py)) * pi / 180.0)
-      modifyRef prevX (\x -> clientX e)
-      modifyRef prevY (\y -> clientY e)
+      modifyRef state (\s -> {  dragged : s.dragged
+                              , prevX   : clientX e
+                              , prevY   : clientY e
+                              , deltaX  : toNumber (clientX e - s.prevX) * pi / 180.0
+                              , deltaY  : toNumber (- (clientY e - s.prevY)) * pi / 180.0
+                              })
     else pure unit
 
-release :: forall e. Ref Boolean -> MouseEvent -> Eff (ref :: REF, console :: CONSOLE | e) Unit
-release dragged e = do
-  modifyRef dragged (\l -> false)
+release :: forall e. Ref State -> MouseEvent -> Eff (ref :: REF, console :: CONSOLE | e) Unit
+release state e = do
+  current <- readRef state
+  modifyRef state (\s -> {  dragged : false
+                          , prevX   : s.prevX
+                          , prevY   : s.prevY
+                          , deltaX  : s.deltaX
+                          , deltaY  : s.deltaY
+                          })
 
 makeEventHandler :: forall e. (MouseEvent -> Eff e Unit) -> EventListener e
 makeEventHandler on = eventListener (\ev -> do
@@ -108,15 +125,15 @@ main = void do
   _ <- setStrokeStyle "#ff0000" ctx
   _ <- setLineWidth 1.0 ctx
 
-  dragged <- newRef false
-  prevX <- newRef 0
-  prevY <- newRef 0
-  deltaX <- newRef 0.0
-  deltaY <- newRef 0.0
+  state <- newRef {  dragged : false
+                  , prevX : 0
+                  , prevY : 0
+                  , deltaX : 0.0
+                  , deltaY : 0.0 }
 
-  addEventHandler "mousedown" $ lock dragged prevX prevY
-  addEventHandler "mousemove" $ rotate dragged prevX prevY deltaX deltaY
-  addEventHandler "mouseup" $ release dragged
+  addEventHandler "mousedown" $ lock state
+  addEventHandler "mousemove" $ rotate state
+  addEventHandler "mouseup" $ release state
 
   let cube = (map $ rotateX (-0.17)) <$> (map $ rotateY 0.17) <$> faces 100.0
-  animate ctx deltaX deltaY dragged cube
+  animate ctx state cube
