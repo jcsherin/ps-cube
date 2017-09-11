5f1f35637e44ae2ec2df0c1af5233021feb2d092
Sherin C <sherin@protoship.io>
2017-09-11 16:40:32 +0530

    Rotate cube when mouse is dragged

--- src/Main.purs
+++ src/Main.purs
@@ -18,24 +18,32 @@ import DOM.Node.Types (elementToEventTarget)
 import Data.Array (head, tail)
 import Data.Either (Either(..))
 import Data.Foldable (for_)
+import Data.Int (toNumber)
 import Data.Maybe (Maybe(..), fromJust)
 import Geometry.Cube (faces, rotateX, rotateY)
-import Geometry.Point (orthographic)
+import Geometry.Point (Point, orthographic)
 import Graphics.Canvas (CANVAS, Context2D, beginPath, clearRect, closePath, getCanvasElementById, getContext2D, lineTo, moveTo, setLineWidth, setStrokeStyle, stroke, translate)
-import Math (round)
+import Math (pi, round)
 import Partial.Unsafe (unsafePartial)
 
-animate :: forall e. Context2D -> Eff (console :: CONSOLE, canvas :: CANVAS, dom :: DOM | e) Unit
-animate ctx = void do
-  drawCube ctx
-  window >>= requestAnimationFrame (animate ctx)
+animate :: forall e. Context2D -> Ref Int -> Ref Int -> Ref Boolean -> Array (Array Point) -> Eff (ref :: REF, console :: CONSOLE, canvas :: CANVAS, dom :: DOM | e) Unit
+animate ctx deltaX deltaY dragged cube = void do
+  _ <- clearRect ctx { x: -300.0, y: -200.0, w: 600.0, h: 400.0}
 
-drawCube :: forall e. Context2D -> Eff (console :: CONSOLE, canvas :: CANVAS, dom :: DOM | e) Unit
-drawCube ctx = void $ do
-  let rotateX' = rotateX 0.15
-  let rotateY' = rotateY (-0.15)
-  let projected = map orthographic <$> map rotateX' <$> map rotateY' <$> faces 80.0
-  for_ projected \face -> do
+  dx <- readRef deltaX
+  dy <- readRef deltaY
+  drg <- readRef dragged
+  let rotateX' = if drg then rotateX $ toNumber (-dy) * pi / 180.0 else rotateX 0.0
+  let rotateY' = if drg then rotateY $ toNumber dx * pi / 180.0 else rotateY 0.0
+  let rotated = if drg then map rotateX' <$> map rotateY' <$> cube else cube
+
+  drawCube ctx rotated
+
+  window >>= requestAnimationFrame (animate ctx deltaX deltaY dragged rotated)
+
+drawCube :: forall e. Context2D -> Array (Array Point) -> Eff (console :: CONSOLE, canvas :: CANVAS, dom :: DOM | e) Unit
+drawCube ctx cube = void $ do
+  for_ (map orthographic <$> cube) \face -> do
 
     let hd = unsafePartial $ fromJust $ head face
     _ <- beginPath ctx
@@ -53,26 +61,19 @@ lock dragged prevX prevY e = do
   modifyRef dragged (\l -> true)
   modifyRef prevX (\x -> clientX e)
   modifyRef prevY (\y -> clientY e)
-  l <- readRef dragged
-  x <- readRef prevX
-  y <- readRef prevY
-  log $ show l <> ", " <> show x <> ", " <> show y
 
-rotate :: forall e. Ref Boolean -> Ref Int -> Ref Int -> MouseEvent -> Eff (ref :: REF, console :: CONSOLE | e) Unit
-rotate dragged prevX prevY e = do
-  x <- readRef prevX
-  y <- readRef prevY
-  let x' = clientX e
-  let y' = clientY e
-  log $ show x <> " -> " <> show x'
-  modifyRef prevX (\x -> x')
-  modifyRef prevY (\y -> y')
+rotate :: forall e. Ref Boolean -> Ref Int -> Ref Int -> Ref Int -> Ref Int -> MouseEvent -> Eff (ref :: REF, console :: CONSOLE | e) Unit
+rotate dragged prevX prevY deltaX deltaY e = do
+  px <- readRef prevX
+  py <- readRef prevY
+  modifyRef deltaX (\x -> clientX e - px)
+  modifyRef deltaY (\y -> clientY e - py)
+  modifyRef prevX (\x -> clientX e)
+  modifyRef prevY (\y -> clientY e)
 
 release :: forall e. Ref Boolean -> MouseEvent -> Eff (ref :: REF, console :: CONSOLE | e) Unit
 release dragged e = do
   modifyRef dragged (\l -> false)
-  l <- readRef dragged
-  log $ show l
 
 makeEventHandler :: forall e. (MouseEvent -> Eff e Unit) -> EventListener e
 makeEventHandler on = eventListener (\ev -> do
@@ -93,7 +94,6 @@ main = void do
   Just canvas <- getCanvasElementById "canvas"
   ctx <- getContext2D canvas
 
-  _ <- clearRect ctx { x: 0.0, y: 0.0, w: 600.0, h: 400.0}
   _ <- translate { translateX: 300.0, translateY: 200.0} ctx
   _ <- setStrokeStyle "#ff0000" ctx
   _ <- setLineWidth 1.0 ctx
@@ -101,9 +101,12 @@ main = void do
   dragged <- newRef false
   prevX <- newRef 0
   prevY <- newRef 0
+  deltaX <- newRef 0
+  deltaY <- newRef 0
 
   addEventHandler "mousedown" $ lock dragged prevX prevY
-  addEventHandler "mousemove" $ rotate dragged prevX prevY
+  addEventHandler "mousemove" $ rotate dragged prevX prevY deltaX deltaY
   addEventHandler "mouseup" $ release dragged
 
-  animate ctx
+  let cube = (map $ rotateX 0.0) <$> (map $ rotateY 0.0) <$> faces 100.0
+  animate ctx deltaX deltaY dragged cube
