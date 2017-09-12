e69d747ab6ec4a5283ed2109caf87e38be470e5c
Sherin C <sherin@protoship.io>
2017-09-12 19:02:12 +0530

    Refactor - extract fn for drawing cube face

--- src/Main.purs
+++ src/Main.purs
@@ -11,13 +11,13 @@ import DOM.HTML.Window (requestAnimationFrame)
 import Data.Array (head, tail)
 import Data.Foldable (for_)
 import Data.Maybe (Maybe(..), fromJust)
-import Geometry.Cube (faces, rotateX, rotateY)
-import Geometry.Point (Point, orthographic)
-import Model (State, decelerate)
 import Events.Mouse (addMouseEventHandler, handleClick, handleMouseDown, handleMouseMove, handleMouseUp)
 import Events.Touch (addTouchEventHandler, handleTouchEnd, handleTouchMove, handleTouchStart)
+import Geometry.Cube (faces, rotateX, rotateY)
+import Geometry.Point (Point, Point2D, orthographic)
 import Graphics.Canvas (CANVAS, Context2D, beginPath, clearRect, closePath, getCanvasElementById, getContext2D, lineTo, moveTo, setLineWidth, setStrokeStyle, stroke, translate)
 import Math (round)
+import Model (State, decelerate)
 import Partial.Unsafe (unsafePartial)
 
 animate :: forall e. Context2D -> Ref State -> Array (Array Point) -> Eff (ref :: REF, console :: CONSOLE, canvas :: CANVAS, dom :: DOM | e) Unit
@@ -36,20 +36,24 @@ animate ctx state cube = void do
                           })
   window >>= requestAnimationFrame (animate ctx state rotated)
 
-drawCube :: forall e. Context2D -> Array (Array Point) -> Eff (console :: CONSOLE, canvas :: CANVAS, dom :: DOM | e) Unit
-drawCube ctx cube = void $ do
-  for_ (map orthographic <$> cube) \face -> do
-
+drawCubeFace :: forall e. Context2D -> Array Point2D -> Eff (console :: CONSOLE, canvas :: CANVAS, dom :: DOM | e) Unit
+drawCubeFace ctx face = do
     let hd = unsafePartial $ fromJust $ head face
+    let tl = unsafePartial $ fromJust $ tail face
+
     _ <- beginPath ctx
     _ <- moveTo ctx (round hd.x) (round $ -hd.y)
-
-    let tl = unsafePartial $ fromJust $ tail face
     for_ tl \p -> do
       lineTo ctx (round p.x) (round $ -p.y)
-
     _ <- closePath ctx
-    stroke ctx
+    _ <- stroke ctx
+
+    pure unit
+
+drawCube :: forall e. Context2D -> Array (Array Point) -> Eff (console :: CONSOLE, canvas :: CANVAS, dom :: DOM | e) Unit
+drawCube ctx cube = void $ do
+  for_ (map orthographic <$> cube) \face ->
+    drawCubeFace ctx face
 
 main :: forall e. Partial => Eff (ref :: REF, console :: CONSOLE, canvas :: CANVAS, dom :: DOM | e) Unit
 main = void do
